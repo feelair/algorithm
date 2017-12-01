6e6c32760eaba20386418de2b615aab8cbde82b9
lujianming <lujianming@cnnic.cn>
2017-12-01 11:15:53 +0800

    init

--- src/main/scala/com/cammsia/number/MaximumSlidingWindow.scala
+++ src/main/scala/com/cammsia/number/MaximumSlidingWindow.scala
@@ -20,6 +20,50 @@ k = 3
 当新值不大于原有最大值时，当滑动新窗口时，仍有可能成为最大值，首先从队列尾依次删除比新值小的数字，然后存储到新值队列尾。
 **/
 
+/**
+package com.rhwayfun.offer;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
+public class MaxNumInWindow {
+
+    public ArrayList<Integer> maxInWindows(int[] num, int size) {
+        ArrayList<Integer> maxList = new ArrayList<Integer>();
+        if(size <= 0) return maxList;
+        //创建一个双端队列保存每个滑动窗口的最大值得下标
+        ArrayDeque<Integer> queue = new ArrayDeque<Integer>();
+        //创建一个变量start用于记录当前滑动窗口的最大值的下标
+        int start = 0;
+        for (int i = 0; i < num.length; i++) {
+            start = i + 1 - size;//当start大于的时候表示第一个窗口已经不能再移动了
+            if(queue.isEmpty()){
+                queue.add(i);
+            }else if(start > queue.peekFirst()){//这个条件表示当前窗口start的值比上一个窗口的start更大
+                queue.pollFirst();
+            }
+
+            while(!queue.isEmpty() && num[queue.peekLast()] <= num[i]){
+                //这种情况表示，队列队尾位置对应的元素比当前元素更小，就移除他，因为需要得到的是窗口最大值
+                queue.pollLast();
+            }
+            queue.add(i);
+            if(start >= 0){
+                //实际上当start=0的时候第第一个滑动窗口，这时队列中保存的是第一个滑动窗口最大值的下标，直接添加就行
+                maxList.add(num[queue.peekFirst()]);
+            }
+        }
+        return maxList;
+    }
+
+    public static void main(String[] args) {
+        int[] num = {2,3,4,2,6,2,5,1};
+        ArrayList<Integer> list = new MaxNumInWindow().maxInWindows(num, 3);
+        System.out.println(list);
+    }
+}
+/**
+
 object MaximumSlidingWindow extends App {
   val num = [1, 2, 7, 7, 8]
   val k = 3
